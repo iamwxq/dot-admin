83442290f8939b32b6ade45b8f902080913d8cbe
iamwxq <whoeverimf5@gmail.com>
2024-06-25 23:37:30 +0800

    chore: add more time enums

--- src/enums/time.ts
+++ src/enums/time.ts
@@ -1,25 +1,52 @@
 /**
+ * @description 毫秒
+ */
+export enum MilliSecsEnum {
+  MS50 = 50,
+  MS100 = 100,
+  MS150 = 150,
+  MS200 = 200,
+  MS250 = 250,
+  MS300 = 300,
+  MS350 = 350,
+  MS400 = 400,
+  MS450 = 450,
+  MS500 = 500,
+  MS550 = 550,
+  MS600 = 600,
+  MS650 = 650,
+  MS700 = 700,
+  MS750 = 750,
+  MS800 = 800,
+  MS850 = 850,
+  MS900 = 900,
+  MS950 = 950,
+}
+
+/**
  * @description 秒
  */
 export enum SecondsEnum {
-  ONE = 1_000,
-  TEN = 10_000,
-  THIRTY = 30_000,
+  S1 = 1_000,
+  S10 = 10_000,
+  S30 = 30_000,
+  S45 = 45_000,
 }
 
 /**
  * @description 分
  */
 export enum MinutesEnum {
-  ONE = 60_000,
-  TEN = 600_000,
-  THIRTY = 1_800_000,
+  M1 = 60_000,
+  M10 = 600_000,
+  M30 = 1_800_000,
+  M45 = 2_500_000,
 }
 
 /**
  * @description 时
  */
 export enum HoursEnum {
-  ONE = 3_600_000,
-  TEN = 30_600_000,
+  H1 = 3_600_000,
+  H12 = 43_200_000,
 }
